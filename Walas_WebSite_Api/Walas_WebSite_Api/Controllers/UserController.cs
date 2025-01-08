003d41c1407226b5160bfd591b9a1edee699bc37
Sebastian Acero Leon <NG28H@techtitute.com>
2025-01-08 15:51:46 -0500

    feat: Configuraciones

---
+++ Walas_WebSite_Api/Walas_WebSite_Api/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Walas_WebSite_Api.Controllers
+{
+    [ApiController]
+    [Route("UserApi/")]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet("GetUser")]
+        public IEnumerable<string> Get()
+        {
+            return ["value1", "value2"];
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("GetUser/{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UserController>
+        [HttpPost("PostUser")]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("PutUser/{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("DeleteUser/{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
